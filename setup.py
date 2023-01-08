9768bad9af58cce73b98e549413a45e05032c9db
mrMekdad <mekdadr6@gmail.com>
2023-01-08 05:51:00 +0100

    metagenome-assembly-notes: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2023-metagenome-assembly-notes",
+    version="0.1.0",
+    description="Assembly tracking helpers with sample manifests and reporting utilities.",
+    author="Red@",
+    packages=["metagenome_assembly_notes"],
+    package_dir={"": "src"},
+)
